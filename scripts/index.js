a6b22ebc76ed2cbfb09611ce77ac038c88a0b857
debajit13 <mallickdebajit3@gmail.com>
2020-06-16 22:21:34 +0530

    rainfall graph added!

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+var dataPoints = [];
+$.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=10&length=10&type=json", function(data) {  
+    $.each(data, function(key, value){
+        dataPoints.push({x: value[0], y: parseInt(value[1])});
+    });
+    chart = new CanvasJS.Chart("chartContainer",{
+        title:{
+            text:"Live Rainfall Data"
+        },
+        data: [{
+        type: "line",
+        dataPoints : dataPoints,
+        }]
+    });
+    chart.render();
+    updateChart();
+});
+
+function updateChart() {
+    $.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json", function(data) {
+        $.each(data, function(key, value) {
+            dataPoints.push({
+                x: parseInt(value[0]),
+                y: parseInt(value[1])
+            });
+       });
+       chart.render();
+       setTimeout(function(){updateChart()}, 1000);
+    });
+ }
